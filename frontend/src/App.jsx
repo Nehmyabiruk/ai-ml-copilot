6567a0e1124b4c47ff591fcb6947483e1c01157a
Nehmyabiruk <mimneh@gmail.com>
2026-08-20 08:39:49 -0400

    changes apibase in frontend app jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import {  useState } from 'react'
-
-const API_BASE = import.meta.env.VITE_API_URL || `${window.location.protocol}//${window.location.hostname}:8000`
+const API_BASE = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000"
 const apiFetch = (path, options = {}) => fetch(path, { ...options, credentials: 'include' })
 
 function repositoryName(url) {
